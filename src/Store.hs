128f89079d03cea61f2b0e1c4155ea20c959ccb4
Sebastiaan Visser <sebas@phony.lan>
2009-02-05 01:27:02 +0100

    Heap monad.

--- src/Store.hs
+++ src/Store.hs
@@ -1,22 +1,28 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Store where
 
+import Control.Monad.State
 import Control.Monad
-import Data.Maybe (isJust)
+import Data.Maybe
+import Data.Record.Label
 import Data.Word
 import IO
 import System.IO
 import qualified Data.ByteString as B
-import qualified Data.IntMap as M
+import qualified Data.ByteString.UTF8 as U
+import qualified Data.IntMap as I
 
 -------------------------------------------------------------------------------
 
-type Offset = Int
-type Size   = Int
-type Header = (Bool, Size)
+type Pointer = Int
+type Size    = Int
+type Header  = (Bool, Size)
 
 data Block =
   Block {
-    offset   :: Offset
+    offset   :: Pointer
   , size     :: Size
   , payload  :: Maybe B.ByteString
   } deriving (Eq, Ord, Show)
@@ -39,22 +45,24 @@ headerSize = 1 + 4
 blockSize :: Block -> Size
 blockSize = (+headerSize) . size
 
-safeOffset :: Handle -> Offset -> IO a -> IO (Maybe a)
+-- TODO handles to the last argument for better currying!
+
+safeOffset :: Handle -> Pointer -> IO a -> IO (Maybe a)
 safeOffset h o c = do
   fs <- fromIntegral `liftM` hFileSize h
   if fs > o then Just `liftM` c else return Nothing
 
-unsafeReadHeader :: Handle -> Offset -> IO Header
+unsafeReadHeader :: Handle -> Pointer -> IO Header
 unsafeReadHeader h o = do
   hSeek h AbsoluteSeek (fromIntegral o)
   x <- read8 h
   s <- read32 h
   return (int2bool x, s)
 
-readHeader :: Handle -> Offset -> IO (Maybe Header)
-readHeader h o = safeOffset h o (unsafeReadHeader h o)
+readHeader :: Pointer -> Handle -> IO (Maybe Header)
+readHeader o h = safeOffset h o (unsafeReadHeader h o)
 
-unsafeReadBlock :: Handle -> Offset -> IO Block
+unsafeReadBlock :: Handle -> Pointer -> IO Block
 unsafeReadBlock h o = do
   (x, s) <- unsafeReadHeader h o
   d <- if x
@@ -62,10 +70,10 @@ unsafeReadBlock h o = do
        else return Nothing
   return (Block o s d)
 
-readBlock :: Handle -> Offset -> IO (Maybe Block)
+readBlock :: Handle -> Pointer -> IO (Maybe Block)
 readBlock h o = safeOffset h o (unsafeReadBlock h o)
 
-writeHeader :: Handle -> Offset -> Header -> IO ()
+writeHeader :: Handle -> Pointer -> Header -> IO ()
 writeHeader h o (x, s) = do
   hSeek h AbsoluteSeek (fromIntegral o)
   write8  h (bool2int x)
@@ -78,61 +86,118 @@ writeBlock h (Block o s d) = do
 
 -------- block allocation and releasing ---------------------------------------
 
-type AllocationMap = M.IntMap Int
+data AbsHeap = 
+  AbsHeap {
+    _allocMap :: I.IntMap [Int]
+  , _heapSize :: Int
+  , _file     :: Handle
+  } deriving Show
 
-readAllocationMap :: Handle -> Offset -> AllocationMap -> IO AllocationMap
-readAllocationMap h o m = do
-  i <- readHeader h o
+$(mkLabels [''AbsHeap])
+
+emptyHeap :: Handle -> AbsHeap
+emptyHeap h = AbsHeap I.empty 0 h
+
+-- statefy these
+insert :: Size -> Pointer -> Heap ()
+insert s o = modM allocMap (I.alter (f o) s)
+  where f x Nothing   = Just [x]
+        f x (Just xs) = Just (x:xs)
+
+delete :: Size -> Heap ()
+delete s = modM allocMap (I.update f s)
+  where f (_:x:xs) = Just (x:xs)
+        f _        = Nothing
+
+grow, shrink :: Int -> Heap ()
+grow   s = modM heapSize (+s)
+shrink s = modM heapSize (-s+)
+
+type Heap m = StateT AbsHeap IO m
+
+accessFile :: (Handle -> IO a) -> Heap a
+accessFile c = getM file >>= liftIO . c
+
+runHeap :: FilePath -> Heap a -> IO a
+runHeap f c = do
+  h <- openBinaryFile f ReadWriteMode
+  (a, _) <- runStateT (represent 0 >> c) (emptyHeap h)
+  return a
+
+represent :: Pointer -> Heap ()
+represent o = do
+  i <- accessFile (readHeader o)
   case i of
-    Just (False, s) -> readAllocationMap h (o + headerSize + s) (M.insert o s m)
-    Just (True,  s) -> readAllocationMap h (o + headerSize + s) m
-    Nothing         -> return m 
+    Nothing -> return ()
+    Just (f, s) -> do
+      when (not f) (insert s o)
+      grow (headerSize + s)
+      represent (o + headerSize + s)
 
-allocate :: Handle -> Offset -> Size ->  IO (Maybe Block)
-allocate h o a = do
-  i <- readHeader h o
-  case i of
-    Nothing                  -> return $ Just (Block o a Nothing) -- eof
-    Just (False, s) | s >= a -> readBlock h o
-    Just (_,     s)          -> allocate h (o + headerSize + s) a
+findFreeBlock :: Size -> Heap (Maybe (Pointer, Size))
+findFreeBlock i = do
+  a <- getM allocMap
+  let min = I.minViewWithKey $ snd $ I.split (i-1) a
+  return $ case min of
+    Just ((s, o:xs), _) -> Just (o, s)
+    _                   -> Nothing
 
-free :: Handle -> Block -> IO ()
-free h b = writeBlock h (b {payload = Nothing})
+allocate :: Size -> Heap Block
+allocate i = do
+  mb <- findFreeBlock i
+  case mb of
+
+    -- No free block found, allocate at end of heap.
+    Nothing -> do
+      e <- getM heapSize
+      grow (headerSize + i)
+      return $ Block e i Nothing
+
+    -- Free block with sufficient size found, reuse this block.
+    Just h@(o, s) -> do
+      delete s
+      return $ Block o s Nothing
+
+free :: Pointer -> Heap ()
+free o = do
+  s <- accessFile $ \h -> do
+    hSeek h AbsoluteSeek (fromIntegral o)
+    write8 h (0::Int)
+    read32 h
+  insert s o
+
+dump :: Heap ()
+dump = do
+  liftIO $ putStrLn "Heap contents:"
+  accessFile (\h -> readBlock h 0 >>= printBlocks h)
 
 fillBlock :: Word8 -> Block -> Block
 fillBlock w b = b { payload = Just (B.replicate (size b) w) }
 
-splitBlock :: Handle -> Size -> Block -> IO ()
-splitBlock h n (Block o s d) = do
-  let a = Block o n Nothing
-      b = Block (o + headerSize + n) (s - n - headerSize) Nothing
-  writeBlock h a
-  writeBlock h b
-
 nextBlock :: Handle -> Block -> IO (Maybe Block)
 nextBlock h b = readBlock h (offset b + blockSize b)
 
+printBlocks :: Handle -> Maybe Block -> IO ()
+printBlocks h Nothing  = return ()
+printBlocks h (Just b) = print b >> nextBlock h b >>= printBlocks h
+
 -------------------------------------------------------------------------------
+
+storeString :: String -> Heap ()
+storeString s = do
+  let bs = U.fromString s
+  b <- allocate (B.length bs)
+  accessFile $ \h -> writeBlock h (b { payload = Just bs })
 
 main :: IO ()
 main = do
-  h <- openBinaryFile "test.db" ReadWriteMode
-  hSetFileSize h 0
-  writeBlock h (Block 0 0 Nothing)
-  allocate h 0 4 >>= maybe (return ()) (writeBlock h . fillBlock 0x34)
-  allocate h 0 6 >>= maybe (return ()) (writeBlock h . fillBlock 0x35)
-  allocate h 0 7 >>= maybe (return ()) (writeBlock h . fillBlock 0x36)
-  allocate h 0 9 >>= maybe (return ()) (writeBlock h . fillBlock 0x37)
-  allocate h 0 3 >>= maybe (return ()) (writeBlock h . fillBlock 0x39)
-  allocate h 0 8 >>= maybe (return ()) (writeBlock h . fillBlock 0x38)
-  readBlock h 25 >>= maybe (return ()) (free h)
-  readBlock h 51 >>= maybe (return ()) (free h)
-  readBlock h 0 >>= printBlocks h
-
-  am <- readAllocationMap h 0 M.empty
-  print am
-
-
-printBlocks h Nothing  = return ()
-printBlocks h (Just b) = print b >> nextBlock h b >>= printBlocks h
+  runHeap "test.db" $ do
+    get >>= liftIO . print 
+    dump
+    storeString "hallo!"
+    storeString "zomaar viel spaß jö!"
+    storeString "haa"
+    get >>= liftIO . print 
+    dump
+  return ()
 
