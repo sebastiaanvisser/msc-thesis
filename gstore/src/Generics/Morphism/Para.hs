1581527067ec65bdfc67d4ac71859805fd846359
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-11-05 17:29:54 +0100

    Removed debug forall.

--- gstore/src/Generics/Morphism/Para.hs
+++ gstore/src/Generics/Morphism/Para.hs
@@ -33,14 +33,14 @@ snd3 (_, y, _) = y
 trd3 :: (a, b, c) -> c
 trd3 (_, _, z) = z
 
-(<++>) :: forall f r a s. (Functor f, Functor (AlgA a f)) => AlgA a f (r -> s) -> AlgA a f r -> AlgA a f (r -> s, r, s)
+(<++>) :: (Functor f, Functor (AlgA a f)) => AlgA a f (r -> s) -> AlgA a f r -> AlgA a f (r -> s, r, s)
 Proj f <++> Proj g = fmap trd3 f <++> fmap trd3 g 
 Psi  f <++> Proj g = Proj (pure id <++> Psi f) <++> Proj g
 Proj f <++> Psi  g = Proj f <++> Proj (pure id <++> Psi g)
 Psi  f <++> Psi  g = Psi (\x -> f (fmap2 fst3 x) `mk` g (fmap2 snd3 x))
   where mk x y = (x, y, x y)
 
-_para :: forall f m a r x. (Traversable f, Lazy m, AnnQ a f m) => (x -> m r) -> (r -> x) -> AlgA a f x -> FixA a f -> m r
+_para :: (Traversable f, Lazy m, AnnQ a f m) => (x -> m r) -> (r -> x) -> AlgA a f x -> FixA a f -> m r
 _para z y (Proj psi) = fmap trd3 . _para (\(a, b, r) -> z r >>= \r' -> return (a, b, r')) (\(a, b, r) -> (a, b, y r)) psi
 _para z y (Psi psi)  = z . psi <=< mapM (g (fmap y . lazy . _para z y (Psi psi))) <=< runQuery
   where g f c = fmap ((,) c) (f c)
