f2e46ad611ef56e911eda43032a1ff1cfe1609f9
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-11-13 13:45:27 +0100

    She is now there.

--- gstore/src/Container/Tree/Cont.hs
+++ gstore/src/Container/Tree/Cont.hs
@@ -1,5 +1,8 @@
+{-# OPTIONS_GHC -F -pgmF she #-}
 module Container.Tree.Cont where
 
+import Control.Applicative
+import Control.Monad
 import Container.Tree.Abstract
 import Prelude hiding (lookup)
 import qualified Generics.Cont as C
@@ -13,37 +16,27 @@ singleton k v p =
      l1 <- p Leaf
      p (Branch k v l0 l1)
 
-triplet :: Monad m => k -> v -> k -> v -> k -> v -> C.Produce a (Tree k v) m
+triplet :: (Applicative m, Monad m) => k -> v -> k -> v -> k -> v -> C.Produce a (Tree k v) m
 triplet a0 b0 a1 b1 a2 b2 p =
-  do l0 <- p Leaf
-     l1 <- p Leaf
-     l2 <- p Leaf
-     l3 <- p Leaf
-     lt <- p (Branch a0 b0 l0 l1)
-     rt <- p (Branch a2 b2 l2 l3)
-     p (Branch a1 b1 lt rt)
+  (| p (| (Branch a1 b1)
+  (| p (| (Branch a0 b0) (p Leaf) (p Leaf) |) @ |)
+  (| p (| (Branch a2 b2) (p Leaf) (p Leaf) |) @ |) |) @ |)
 
-lookup :: (Monad m, Ord k) => k -> C.Query a (Tree k v) m (Maybe v)
-lookup _ _ Leaf = return Nothing
+lookup :: (Monad m, Applicative m, Ord k) => k -> C.Query a (Tree k v) m (Maybe v)
+lookup _ _ Leaf = (| Nothing |)
 lookup k q (Branch c d l r) =
   case k `compare` c of
-    EQ -> return (Just d)
+    EQ -> (| (Just d) |)
     LT -> q l
     GT -> q r
 
-size :: (Num c, Monad m) => C.Query a (Tree k b) m c
-size _ Leaf = return 0
-size q t =
-  do k <- q (leftT  t)
-     v <- q (rightT t)
-     return (1 + k + v)
+size :: (Num c, Applicative m) => C.Query a (Tree k b) m c
+size _ Leaf =    (| 0 |)
+size q t    = (| (| 1 |) + (| q (leftT t) + q (rightT t) |) |)
 
-depth :: (Ord c, Num c, Monad m) => C.Query a (Tree k v) m c
-depth _ Leaf = return 0
-depth q t =
-  do k <- q (leftT  t)
-     v <- q (rightT t)
-     return (1 + max k v)
+depth :: (Ord c, Num c, Applicative m, Monad m) => C.Query a (Tree k v) m c
+depth _ Leaf =    (| 0 |)
+depth q t    = (| (| 1 |) + (| max (q (leftT t)) (q (rightT t)) |) |)
 
 alter :: (Monad m, Ord k) => (Maybe v -> Maybe v) -> k -> C.Modify a (Tree k v) m
 alter f k _ p Leaf = maybe (p Leaf) (\v -> singleton k v p) (f Nothing)
